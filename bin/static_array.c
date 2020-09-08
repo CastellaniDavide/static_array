1e7ae234b89b3bdf34bc39fefef4c80211b89d19
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-08 14:32:37 +0200

    Created bin/template.c

---
+++ bin/static_array.c
@@ -0,0 +1,22 @@
+/**
+ * @file static_array.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief 
+ *
+ * @ingroup static_array
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("static_array");
+	return 0;
+}
